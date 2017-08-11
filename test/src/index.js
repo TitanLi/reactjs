a94ded06db1f341c676df10b63d0795d17de7aaa
TitanLi <Lisheng0706@gmail.com>
2017-08-11 13:18:41 +0800

    Add hello

--- test/src/index.js
+++ test/src/index.js
@@ -4,8 +4,31 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+class Name extends React.Component{
+  render(){
+    return <h1>Google</h1>
+  }
+}
+
+class GoogleUrl extends React.Component{
+  render(){
+    return <a href={this.props.googleUrl}>{this.props.googleUrl}</a>
+  }
+}
+
+class Main extends React.Component{
+  render(){
+    return (
+      <div>
+        <Name />
+        <GoogleUrl googleUrl={this.props.googleUrl}/>
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <App />,
+  <Main googleUrl="http://www.google.com"/>,
   document.getElementById('root')
 );
 registerServiceWorker();
