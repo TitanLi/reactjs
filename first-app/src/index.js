b58f2630b9f5f7df76b638d6ba4adbb0b8e9e65d
TitanLi <lisheng0706@gmail.com>
2020-01-29 15:33:07 +0800

    Add material example

--- first-app/src/index.js
+++ first-app/src/index.js
@@ -10,68 +10,74 @@ import ToggleButton from './ToggleButton';
 import LoginControl from './LoginControl';
 import MapList from './MapList';
 import Form from './Form';
+import Dashboard from './material/Dashboard';
 import * as serviceWorker from './serviceWorker';
 
 function tick() {
     ReactDOM.render(<App />, document.getElementById('root'));
 }
-setInterval(tick, 1000);
+// setInterval(tick, 1000);
+
+// ReactDOM.render(
+//     <AppWelcome />,
+//     document.getElementById('say')
+// );
+
+// const comment = {
+//     date: new Date(),
+//     text: 'I hope you enjoy learning React!',
+//     author: {
+//         name: 'Hello Kitty',
+//         avatarUrl: 'https://placekitten.com/g/64/64',
+//     },
+// };
+
+// ReactDOM.render(
+//     <AppComment
+//         date={comment.date}
+//         text={comment.text}
+//         author={comment.author}
+//     />,
+//     document.getElementById('comment')
+// );
+
+// function classClock() {
+//     ReactDOM.render(
+//         <ClassClock />,
+//         document.getElementById('classClock')
+//     );
+// }
+
+// setInterval(classClock, 1000);
+
+// ReactDOM.render(
+//     <ActionLink />,
+//     document.getElementById('actionLink')
+// )
+
+// ReactDOM.render(
+//     <ToggleButton />,
+//     document.getElementById('toggleButton')
+// )
+
+// ReactDOM.render(
+//     <LoginControl  isLoggedIn={true}/>,
+//     document.getElementById('loginControl')
+// )
+
+// ReactDOM.render(
+//     <MapList number={[1, 2, 3, 4]}/>,
+//     document.getElementById('mapList')
+// )
+
+// ReactDOM.render(
+//     <Form/>,
+//     document.getElementById('form')
+// )
 
 ReactDOM.render(
-    <AppWelcome />,
-    document.getElementById('say')
-);
-
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'https://placekitten.com/g/64/64',
-    },
-};
-
-ReactDOM.render(
-    <AppComment
-        date={comment.date}
-        text={comment.text}
-        author={comment.author}
-    />,
-    document.getElementById('comment')
-);
-
-function classClock() {
-    ReactDOM.render(
-        <ClassClock />,
-        document.getElementById('classClock')
-    );
-}
-
-setInterval(classClock, 1000);
-
-ReactDOM.render(
-    <ActionLink />,
-    document.getElementById('actionLink')
-)
-
-ReactDOM.render(
-    <ToggleButton />,
-    document.getElementById('toggleButton')
-)
-
-ReactDOM.render(
-    <LoginControl  isLoggedIn={true}/>,
-    document.getElementById('loginControl')
-)
-
-ReactDOM.render(
-    <MapList number={[1, 2, 3, 4]}/>,
-    document.getElementById('mapList')
-)
-
-ReactDOM.render(
-    <Form/>,
-    document.getElementById('form')
+    <Dashboard/>,
+    document.getElementById('title')
 )
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
